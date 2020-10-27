ed4a92022a11c2c11a3341958a4c3af3f544d990
Carlos Patino <carlospatinos@gmail.com>
2020-10-27 11:29:13 +0000

    Adding ci feedback loop

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var SocketSingleton = require('../utils/socket-singleton');
+//var SocketSingleton = require('../utils/socket-singleton');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -14,6 +14,12 @@ router.get('/signin', function (req, res, next) {
 
 router.post('/signin', function (req, res, next) {
   res.render('welcome', { title: 'Scrum' });
+});
+
+router.get('/appRunning', function (req, res, next) {
+  return res.status(200).json({
+    message: "Application is running",
+  });
 });
 
 module.exports = router;
