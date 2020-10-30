72de4e197972839d0da682534f5eb162a1dd40f3
Carlos Patino <carlospatinos@gmail.com>
2020-10-30 21:03:36 +0000

    creating login page

--- packages/scrum-client/src/App.js
+++ packages/scrum-client/src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./content/Home";
+import Login from "./content/Login";
+import NotFound from "./content/NotFound";
+
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <main >
-        <Container className="px-0">
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
-          </Switch>
-        </BrowserRouter>
+        <Container className="px-5">
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <NotFound />
+            </Switch>
+          </BrowserRouter>
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- packages/scrum-client/src/App.test.js
+++ packages/scrum-client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders planning text', () => {
+test('renders menu', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Planning/i);
+  const linkElement = screen.getByText(/Signup/i);
   expect(linkElement).toBeInTheDocument();
 });
