3e58b2f535ea666b76628b8e1105bbc8c378402a
Carlos Patino <carlospatinos@gmail.com>
2020-11-01 20:48:46 +0000

    creating constant file

--- packages/scrum-client/src/content/Login/Login.js
+++ packages/scrum-client/src/content/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Alert,
   Button,
@@ -6,31 +6,30 @@ import {
   FormControl,
   FormLabel,
 } from "react-bootstrap";
+import {API_BASE_URL, ACCESS_TOKEN_NAME} from '../../constants/apiConstants';
+
 import "./Login.css";
 
-import TagManager from "react-gtm-module";
+// import TagManager from 'react-gtm-module'
 
-if (process.env.NODE_ENV === "production" && !!process.env.REACT_APP_GTM_ID) {
-  const tagManagerArgs = {
-    dataLayer: {
-      page: "login", //Specific to each page
-      pagePath: window.location.pathname + window.location.search, //"/login", //Specific to each page
-      title: "login",
-    },
-    dataLayerName: "PageDataLayer",
-  };
-  TagManager.dataLayer(tagManagerArgs);
-}
+// if (process.env.NODE_ENV === "development" && !!process.env.REACT_APP_GTM_ID) {
+//   document.title = "login";
+//   const tagManagerArgs = {
+//     dataLayer: {
+//       page: "login", //Specific to each page
+//       pagePath: window.location.pathname + window.location.search, //"/login", //Specific to each page
+//       title: "login"
+//     },
+//     dataLayerName: "PageDataLayer"
+//   };
+//   TagManager.dataLayer(tagManagerArgs);
+// }
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [apiResponse, setApiResponse] = useState();
-
-  // useEffect(() => {
-  //   // Update the document title using the browser API
-  //document.title = `You clicked ${count} times`;
-  // });
+  const [apiResponse, setApiResponse] = useState("");
+  
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -46,11 +45,18 @@ export default function Login() {
     };
 
     try {
-      fetch(process.env.REACT_APP_API_URL + "/api/login", requestOptions) ///api
+      fetch(API_BASE_URL + "/api/login", requestOptions) ///api
         .then((response) => response.json())
         //.then(response => response.text())
         .then((data) => {
-          setApiResponse(data.message);
+          console.log(data);
+          if (data.isAuth){
+            console.log("user authenticated");
+            localStorage.setItem(ACCESS_TOKEN_NAME, data.token);
+            //redirectToHome();
+          } else {
+            setApiResponse(data.message);
+          }
         });
     } catch (e) {
       console.log(e);
--- packages/scrum-client/src/content/NotFound/NotFound.js
+++ packages/scrum-client/src/content/NotFound/NotFound.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./NotFound.css";
 
-import TagManager from 'react-gtm-module'
+// import TagManager from 'react-gtm-module'
 
 
-if (process.env.NODE_ENV === "production" && !!process.env.REACT_APP_GTM_ID) {  
-  const tagManagerArgs = {
-    dataLayer: {
-      page: "not_found", //Specific to each page
-      pagePath: window.location.pathname + window.location.search, //"/login", //Specific to each page
-      title: "not found"
-    },
-    dataLayerName: "PageDataLayer"
-  };
-  TagManager.dataLayer(tagManagerArgs);
-}
+// if (process.env.NODE_ENV === "development" && !!process.env.REACT_APP_GTM_ID) {  
+//   document.title = "not_found";
+//   console.log(document.title);
+//   const tagManagerArgs = {
+//     dataLayer: {
+//       page: "not_found", //Specific to each page
+//       pagePath: window.location.pathname + window.location.search, //"/login", //Specific to each page
+//       title: "not found"
+//     },
+//     dataLayerName: "PageDataLayer"
+//   };
+//   TagManager.dataLayer(tagManagerArgs);
+// }
 
 
 export default function NotFound() {
