1f6ee2e29e3194092fe803366b187b4ae2ff8810
Carlos Patino <carlospatinos@gmail.com>
2020-12-17 14:37:38 +0000

    fix: troubleshooting memory issues

--- packages/scrum-server/utils/db.js
+++ packages/scrum-server/utils/db.js
@@ -7,8 +7,6 @@ const db_name = process.env.DB_NAME;
 
 const DBConnection = require('mongoose');
 
-// mongodb+srv://mongoUser:<password>@cluster0.2fgfs.mongodb.net/<dbname>?retryWrites=true&w=majority
-//DBConnection.connect(`mongodb://${db_user}:${db_pass}@${db_url}/${db_name}`)
 DBConnection.connect(`mongodb+srv://${db_user}:${db_pass}@${db_url}/${db_name}?retryWrites=true&w=majority`)
   .then(() => console.log(`Connected to ${db_url}`))
   .catch(err => console.log(err));
