7ee60aaffc5916deb81003ac18347b51177b92a9
Carlos Patino <carlospatinos@gmail.com>
2020-12-27 11:59:04 +0000

    fix: technical debt, defulat page is login now and menu hides and shows based on auth user

--- packages/scrum-client/src/components/Header.js
+++ packages/scrum-client/src/components/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-// import { useAppContext } from '../lib/contextLib';
 import PATHS from '../constants/paths';
 import routes from '../config/routes';
 import { useAuthState } from '../context';
 
 export default function Header() {
-  // const { isAuthenticated } = useAppContext();
   const userDetails = useAuthState();
   return (
     <header>
@@ -16,13 +14,21 @@ export default function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end" activeKey={PATHS.LOGIN}>
-              {routes.map(route => {
-                return route.isPrivate && !userDetails.login_access_token ? (
-                  <p />
-                ) : (
-                  <Nav.Link href={route.path}>{route.title}</Nav.Link>
-                );
-              })}
+              {!userDetails.login_access_token
+                ? routes.map(route => {
+                    return route.isPrivate || !route.isVisibleOnMenu ? (
+                      <div />
+                    ) : (
+                      <Nav.Link href={route.path}>{route.title}</Nav.Link>
+                    );
+                  })
+                : routes.map(route => {
+                    return route.isPrivate && route.isVisibleOnMenu ? (
+                      <Nav.Link href={route.path}>{route.title}</Nav.Link>
+                    ) : (
+                      <div />
+                    );
+                  })}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
