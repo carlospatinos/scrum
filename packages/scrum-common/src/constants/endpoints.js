f5bd40e24c95f2b9dcc73ffd77da97f42bc70d71
christopher-chavez <doncesarts@gmail.com>
2020-12-27 23:04:32 +0000

    feat(scrum-common): adds common package for server and client

---
+++ packages/scrum-common/src/constants/endpoints.js
@@ -0,0 +1,15 @@
+const END_POINTS = {
+  AUTH: '/auth',
+  AUTH_LOCAL: '/local',
+  AUTH_GOOGLE: '/google',
+  AUTH_FACEBOOK: '/facebook',
+  AUTH_TWITTER: '/twitter',
+  API: '/api',
+  SIGN_UP: '/signup',
+  PLANNING_SESSION: '/planningsession',
+  LOGOUT: '/logout',
+  PROFILE: '/profile',
+  ROOT: '/',
+};
+
+module.exports = END_POINTS;
