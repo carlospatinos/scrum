a9a4b8e59a555ef0686583a1487f70317cf72b81
christopher-chavez <doncesarts@gmail.com>
2021-01-04 10:05:37 +0000

    refactor(scrum-server): refactor UserController.signUp

--- packages/scrum-server/src/api/components/planningSession/controller.js
+++ packages/scrum-server/src/api/components/planningSession/controller.js
@@ -1,4 +1,3 @@
-// controller.ts
 // The controller class handles incoming requests, validates them and sends the response data back to the client. It uses the service class to interact with the database.
 
 const PlanningSessionService = require('./service');
--- packages/scrum-server/src/api/components/user/controller.js
+++ packages/scrum-server/src/api/components/user/controller.js
@@ -1,2 +1,18 @@
 // controller.ts
 // The controller class handles incoming requests, validates them and sends the response data back to the client. It uses the service class to interact with the database.
+
+const UserService = require('./service');
+
+const signUp = async (req, res) => {
+  try {
+    const serviceResponse = await UserService.signUp(req);
+    return res.status(200).json({
+      success: true,
+      user: serviceResponse.user,
+    });
+  } catch (e) {
+    return res.status(400).json({ success: false, message: e.message });
+  }
+};
+
+module.exports = { signUp };
