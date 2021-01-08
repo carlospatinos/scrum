090d98940c05c41234ee6c95045950b65eaefb94
Carlos Patino <carlospatinos@gmail.com>
2021-01-08 09:49:41 +0000

    feat: adding links to the home page

--- packages/scrum-client/src/content/Home/Home.js
+++ packages/scrum-client/src/content/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, CardDeck, Card } from 'react-bootstrap';
+import { useLocation, Link } from 'react-router-dom';
+import { PATHS } from '../../constants';
 
 // import TagManager from 'react-gtm-module'
 
@@ -18,6 +20,9 @@ import { Container, CardDeck, Card } from 'react-bootstrap';
 // }
 
 const Home = () => {
+  const location = useLocation();
+  const redirectedFrom = location.state?.redirectedFrom?.pathname || PATHS.HOME;
+
   return (
     <Container>
       <br />
@@ -38,18 +43,35 @@ const Home = () => {
             <h4>Planning</h4>
           </Card.Header>
           <Card.Body>
-            <Card.Title />
-            <Card.Text />
+            <Card.Title>Ready to start?</Card.Title>
+            <Card.Text>
+              <Link
+                to={{
+                  pathname: PATHS.PLANNING_CONFIG,
+                  state: { redirectedFrom: { pathname: redirectedFrom } },
+                }}
+              >
+                Config a session
+              </Link>
+            </Card.Text>
           </Card.Body>
         </Card>
-        <Card>
+        <Card style={{ width: '10rem' }}>
           <Card.Header>
             {' '}
-            <h4>Other</h4>
+            <h4>Looking for more?</h4>
           </Card.Header>
+          <Card.Img
+            variant="top"
+            src="https://play-lh.googleusercontent.com/AECLjXX7mj2l13K2oOA3B9cSgEbIn30gs2UrT5vmOWjFuFbXGpyc_2vcwOmCjEVp=s140-rw"
+          />
           <Card.Body>
-            <Card.Title />
-            <Card.Text />
+            <Card.Title>We have also an android app</Card.Title>
+            <Card.Text>
+              <a href="https://play.google.com/store/apps/details?id=com.kinnettik.scrumtools">
+                Scrum Tools
+              </a>
+            </Card.Text>
           </Card.Body>
         </Card>
       </CardDeck>
