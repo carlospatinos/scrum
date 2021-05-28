99f0dfc69da5246eed9b6b9619a825ae9db579fc
Carlos Patino <carlospatinos@gmail.com>
2021-05-28 15:19:08 +0100

    fix: removing cleanRepo script

--- packages/scrum-client/src/api/ProfileAPI.js
+++ packages/scrum-client/src/api/ProfileAPI.js
@@ -3,7 +3,6 @@ import { API_CONSTANTS } from '../constants';
 import { Request } from '../util';
 
 const remove = async payload => {
-  // TODO - this request should be type delete
   return Request.del(
     `${API_CONSTANTS.API_BASE_URL}${END_POINTS.API}${END_POINTS.DELETE_PROFILE}`,
     payload
