7cb377ba81a039d1faadd20e4502e316364b3606
Carlos Patino <carlospatinos@gmail.com>
2021-05-28 15:09:49 +0100

    fix: testing del instead of post

--- packages/scrum-client/src/tests/api/ProfileAPI.js.test.js
+++ packages/scrum-client/src/tests/api/ProfileAPI.js.test.js
@@ -5,11 +5,11 @@ import { API_CONSTANTS } from '../../constants';
 
 describe('ProfileAPI', () => {
   test('remove  profile request', () => {
-    Request.post = jest.fn();
+    Request.del = jest.fn();
     const payload = {};
     ProfileAPI.remove(payload);
 
-    expect(Request.post).toHaveBeenCalledWith(
+    expect(Request.del).toHaveBeenCalledWith(
       `${API_CONSTANTS.API_BASE_URL}${END_POINTS.API}${END_POINTS.DELETE_PROFILE}`,
       payload
     );
