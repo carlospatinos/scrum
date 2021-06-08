2306022174ae53ef60c772314bd95ea465f3db1e
Carlos Patino <carlospatinos@gmail.com>
2021-06-08 12:50:35 +0100

    fix: adding more logging and a test

--- packages/scrum-server/src/services/socket/ServerSocketState.js
+++ packages/scrum-server/src/services/socket/ServerSocketState.js
@@ -27,6 +27,7 @@ const ServerSocketState = (initialState = []) => {
     if (!_room.users.has(userId)) {
       logger.debug(`assignUserToRoom adding user {${userId}} to the room {${room.id}}`);
       _room.users.set(userId, user);
+      logger.silly(`assignUserToRoom adding user {${userId}} to the room {${room.id}} resulting in ${JSON.stringify(_room.users)}`);
     }
     return rooms.get(room.id);
   };
@@ -45,6 +46,7 @@ const ServerSocketState = (initialState = []) => {
     logger.debug(`setRoomStoryVote on room {${room.id}} for user {${userId}} with value {${vote}}`);
     const _room = rooms.get(room.id);
     _room.storyVotes.set(userId, vote);
+    logger.silly(`setRoomStoryVote on room {${room.id}} for user {${userId}} has {${JSON.stringify(_room.storyVotes)}}`);
     return _room.storyVotes;
   };
   const getRoom = room => {
---
+++ packages/scrum-server/tests/babelTranspileTest.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+
+describe('Map', () => {
+  describe('set', () => {
+    it('should add an element to the map', done => {
+      const k1 = { id: 1 };
+      const k2 = { id: 2 };
+      const storyVotes = new Map();
+      storyVotes.set(k1.id, k1);
+      const foundK1 = storyVotes.get(k1.id);
+      storyVotes.should.have.key(k1.id);
+      storyVotes.should.not.have.key(k2.id);
+      assert.equal(k1, foundK1);
+      done();
+    });
+  });
+});
+
+describe('Array', () => {
+  describe('find', () => {
+    it('should return an element to the array', done => {
+      const storyVotes = [5, 12, 8, 130, 44];
+      const idToFind = 8;
+      const found = storyVotes.find(id => id === idToFind);
+      console.log(found);
+      // assert.equal(found, idToFind);
+      done();
+    });
+  });
+});
+
+const inventory = [
+  { name: 'apples', quantity: 2 },
+  { name: 'cherries', quantity: 8 },
+  { name: 'bananas', quantity: 0 },
+  { name: 'cherries', quantity: 5 },
+  { name: 'cherries', quantity: 15 },
+];
+
+const result = inventory.find(({ name }) => name === 'cherries');
+
+console.log(result); // { name: 'cherries', quantity: 5 }
