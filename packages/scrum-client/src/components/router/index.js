c982038ca38a827ddefe07bcaaa29d8dcc43c3a8
Carlos Patino <carlospatinos@gmail.com>
2021-06-10 10:15:59 +0100

    fix(scrum-client): removing unused files

--- packages/scrum-client/src/components/router/index.js
+++ packages/scrum-client/src/components/router/index.js
@@ -1,5 +1,3 @@
-import AuthenticatedRoute from './AuthenticatedRoute';
-import TitledRoute from './TitledRoute';
 import JoinSessionRoute from './JoinSessionRoute';
-// TODO REMOVE ENTIRE FILE
-export { TitledRoute, AuthenticatedRoute, JoinSessionRoute };
+
+export default { JoinSessionRoute };
