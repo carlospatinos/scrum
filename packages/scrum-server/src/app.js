b69a7c145dabbd7079fd44b6f58f527a1e811300
Carlos Patino <carlospatinos@gmail.com>
2021-06-17 16:56:47 +0100

    fix: fixes #116

--- packages/scrum-server/src/app.js
+++ packages/scrum-server/src/app.js
@@ -21,6 +21,9 @@ const app = express();
 const i18n = require('i18n');
 const { TipsRoutes, UserRoutes, PlanningSessionRoutes, AuthRoutes , DefaultRoute, UserStoryRoutes } = require('./api/routes');
 
+
+console.log("--->>>>>>" + __dirname + "/locales")
+
 AuthStrategies.configure(passport);
 i18n.configure({
   locales: ['es', 'en'],
